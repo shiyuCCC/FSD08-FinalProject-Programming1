8dd5aff44a71ae14b678d2e8b5711c34d18c2a10
ShiyuCCC <115126635+shiyuCCC@users.noreply.github.com>
2023-03-05 22:35:16 -0500

    Method 1-5

--- JavaSourceCode/Validator.java
+++ JavaSourceCode/Validator.java
@@ -3,7 +3,51 @@ public class Validator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		System.out.print(singleAtSign("A@b@c@d"));
 
 	}
+	
+	public static boolean isAlphaNum(char input) {
+		
+		//check if the input is alphanumeric.
+		
+		return (input >= 'a'&& input<='z')||(input >= 'A' && input<='Z')||(input>='0'&&input<='9');
+		
+	}
+	
+	
+	public static boolean isSpecialChar(char a, boolean b) {
+		return ((a == '-' || a =='.') || (a == '_' && b == true));
+		
+	}
+	
+	
+	public static boolean isPrefixChar(char a) {
+		
+		if ((isAlphaNum(a)== true) || (isSpecialChar(a, true)==true)) {
+			return true;
+		}else return false;
+	}
+	
+	
+	public static boolean isDomainChar(char a) {
+		if ((isAlphaNum(a)== true) || (isSpecialChar(a, false)==true)) {
+			return true;
+		}else return false;
+		
+	}
 
+	
+	public static boolean singleAtSign(String a) {
+		int count = 0;
+		//for loop to check each char in the string and record the times of @
+		for ( int i =0;i<a.length();i++) {
+			if (a.charAt(i) == '@') {
+				count++;} else;
+		}
+		return (count == 1);
+	}
+	
 }
